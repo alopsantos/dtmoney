63530d83edb0fc7f275beccc598167a33ec49971
Anderson Lopes dos Santos <anderson_lops@hotmail.com>
2022-01-15 19:30:14 -0300

    Api axios and modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface IHeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: IHeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
 
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
---
+++ src/services/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
+// export const api = axios.create({
+//   baseURL: "https://ander.lopscorp.com",
+// });
+
+// api.interceptors.request.use(function (config) {
+//   const token =
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDIyODMzMDYsImV4cCI6MTY0MjI4NjkwNiwic3ViIjoiNTIwODAyODEtNTRiNy00MjdkLWFmNmQtZDA1NzkyZDRmNGM5In0.BLDfMoGv2BPLKSmPCPucZWPdMAqAmbuKku3TiOaYbAY";
+//   config.headers.Authorization = token ? `Bearer ${token}` : "";
+//   return config;
+// });
